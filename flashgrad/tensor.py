02615b5c23a628059772d04852a07203bd863c64
Less Wright <lessw@etrillium.com>
2023-06-18 08:14:33 -0700

    add add, mul, pow, relu

--- flashgrad/tensor.py
+++ flashgrad/tensor.py
@@ -31,6 +31,49 @@ class Tensor:
  
   def __repr__(self):
     return f"FlashTensor {self.data=} with {self.grad=}")
+  
+  def __add__(self, other):
+    if other not isinstance(other, Tensor):
+      other = Tensor(other)
+
+    out = Tensor(self.data + other.data, (self, other), '+')
+    def _backward():
+      self.grad+=out.grad
+      other.grad+=out.grad
+    out._backward = _backward
+    
+    return out
+  
+  def __mul__(self, other):
+    if other not isinstance(other, Tensor):
+      other = Tensor(other)
+    out = Tensor(self.data * other.data, (self, other), "*")
+    
+    def _backward():
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
+     out._backward = _backward
+    
+    return out
+  def __pow__(self,other):
+    assert isinstance(other, (int, float)), "only int or float powers supported"
+    out = Tensor(self.data**other, (self,),f"**{other}")
+    
+    def _backward():
+      self.grad +=(other* self.data**(other-1))* out.grad
+     out._backward = _backward
+    
+    return out
+  
+  def relu(self):
+    out = Tensor(0 if self.data <0 else self.data, (self,), 'ReLU')
+    
+    def _backward():
+      self.grad += (out.data > 0)* out.grad
+      
+    out._backward = _backward
+    return out
+  
     
   def backward(self):
     if self.data.shape > 1:
@@ -43,7 +86,7 @@ class Tensor:
               visited.add(v)
               for child in v._prev:
                 build_topo(child)
-              topp.append(v)
+              topo.append(v)
      build_topo(self)
      
      # one Tensor at a time, apply chain rule to get gradient
